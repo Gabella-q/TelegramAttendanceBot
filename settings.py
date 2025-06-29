322f9c21376359e4c1e4b7062933245e629e9ddb
Gabella-q <sorokinvladimir540@gmail.com>
2025-06-29 21:16:41 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ SUPER_HR = {
     "employee_id": os.environ.get("SUPER_HR_EMP_ID"),
     "fullname": os.environ.get("SUPER_HR_NAME"),
     "role": "HR",
-    "temp_pwd": os.environ.get("SUPER_HR_PWD"),
+    "temp_pwd": "12345"
     "last_chat_id": "",
     "is_active": True,
     "is_pwd_expired": False,
